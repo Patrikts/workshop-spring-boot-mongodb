4cb56711796b25331e56f995ccbfabc580aeebe3
Patrik <trikteixeira@gmail.com>
2023-07-29 12:15:49 -0300

    Projeção dos dados do author com DTO

--- src/main/java/com/nelioalvez/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalvez/workshopmongo/config/Instantiation.java
@@ -34,7 +34,7 @@ public class Instantiation implements CommandLineRunner {
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
